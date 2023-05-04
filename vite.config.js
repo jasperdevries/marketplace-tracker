1532894c4dd937464add1d92028a6d9fbf09a9a6
Jasper de Vries <j.devries@exapps.nl>
2023-05-04 16:58:13 +0200

    add vite-serve to ddev

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        server: {
+            hmr: {
+                protocol: 'wss',
+                host: env.VITE_APP_URL,
             },
-        }),
-    ],
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    };
 });
