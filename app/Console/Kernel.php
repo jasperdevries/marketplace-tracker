d5930f582c38db141ecd94fcfda354fe6ccc3622
Jasper de Vries <j.devries@exapps.nl>
2023-05-04 16:28:35 +0200

    schedule get-prices

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('backup:run')->daily()->at('01:30');
+        $schedule->command('items:get-prices')->everyThreeHours();
     }
 
     /**
